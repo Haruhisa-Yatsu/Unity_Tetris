a9c99c2562a13d92dff39becc18b77c8095f34e9
HaruhisaYatsu <yatsu.haruhisa@nsg.gr.jp>
2024-06-20 12:27:29 +0900

    ブロック一つだけ配置

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Board : MonoBehaviour
 {
+    public Transform _minoRoot;
+
+    public Mino _mino;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Mino.cs
+++ Assets/Scripts/Mino.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Mino : MonoBehaviour
 {
+    private RectTransform rectTransform;
+
+    /// <summary>
+    /// ブロックのプレハブ　Inspectorから指定する
+    /// </summary>
+    public Block blockPrefab;
+
+    /// <summary>
+    /// ミノを構成しているブロックたち
+    /// </summary>
+    private Block[] blocks = new Block[1];
+
+    private int posX;
+    private int posY;
+
+    private readonly int startPosX = 5;
+    private readonly int startPosY = -1;
+
+  
+
+    public void CreateBlocks()
+    {
+        if (blocks[0] != null)
+        {
+            Destroy(blocks[0].gameObject);
+            blocks[0] = null;
+        }
+
+        posX = startPosX;
+        posY = startPosY;
+
+        var block = Instantiate(blockPrefab, transform);
+
+        block.SetColor(
+            new Color(
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f),
+                Random.Range(0.0f, 1.0f)
+                )
+            );
+
+        blocks[0] = block;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rectTransform = GetComponent<RectTransform>();
+        CreateBlocks();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rectTransform.anchoredPosition = new Vector3(posX * 100.0f, posY * 100.0f, 0.0f);
     }
 }
